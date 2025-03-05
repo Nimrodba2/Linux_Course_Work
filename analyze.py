cb0b907d9d00f9effc9d85cee89970cb57540e5e
Nimrod Barzilay <nimrodba2@gmail.com>
2025-03-05 05:32:40 +0200

    Ran the analyze.py script

--- analyze.py
+++ analyze.py
@@ -5,10 +5,3 @@ data = pd.read_csv('DOWNLOADS/LINUX_COURSE_WORK-downloads/plants.csv')
 
 # Print first 5 rows to check
 print(data.head())
-
-python3 ~/Linux_Course_Work/analyze.py
-git add analyze.py
-git commit -m "Created analyze.py for analyzing plants data"
-git push -u origin main
-
-
